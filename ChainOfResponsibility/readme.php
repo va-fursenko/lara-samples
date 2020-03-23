4de33989c085134193d715c7f4751d0a86d672dd
Виктор Фурсенко <va.fursenko@yandex.ru>
2020-03-23 13:40:15 +0300

    Update readme.php

--- ChainOfResponsibility/readme.php
+++ ChainOfResponsibility/readme.php
@@ -18,3 +18,21 @@ $builder = $filtrator->filter($builder);
 // Фильтирация результатов при поиске отелей
 $filtrator = app()->make(IHotelsSearchResultsFiltrator::class, [$request, $this->isLite()]);
 $results = $filtrator->filter($results);
+
+
+// Организуем сервис-провайдер для гибкого DI
+class HotelsSearchResultsFiltratorServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        $this->app->bind(IHotelsSearchResultsFiltrator::class, function ($app, $params) {
+            if (count($params) < 2) {
+                throw new \Exception(IHotelsSearchResultsFiltrator::class . " app::make() needs 2 parameters: (HotelSearchRQ)'request', (bool)'isLite'");
+            }
+            if (empty($params[0]) || !$params[0] instanceof HotelSearchRQ) { 
+                throw new \Exception(IHotelsSearchResultsFiltrator::class . " app::make() needs " . HotelSearchRQ::class . ' parameter');
+            }
+            return new HotelsSearchResultsFiltrator($params[0], (bool)$params[1]);
+        });
+    }
+}
